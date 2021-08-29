3fb3d00b00ef98a0bd702559dc61b54bf294050e
climberbrad <climberbrad@gmail.com>
2021-08-28 22:31:40 -0600

    new handler, DELETE and PUT

---
+++ posts.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Cow struct {
+	Name   string `json:"name"`
+	ID     string `json:"id"`
+	Date   string `json:"date"`
+	Image  string `json:"image"`
+	Finder string `json:"finder"`
+}
+
+type postsResource struct{
+	sync.Mutex
+	store map[string]Cow
+}
+
+func newHandler() *postsResource {
+	return &postsResource{
+		store: map[string]Cow{},
+	}
+}
+
+func (rs postsResource) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Get("/", rs.List)    // GET /posts - Read a list of posts.
+	r.Post("/", rs.Create) // POST /posts - Create a new post.
+
+	r.Route("/{id}", func(r chi.Router) {
+		r.Use(PostCtx)
+		r.Get("/", rs.Get)       // GET /posts/{id} - Read a single post by :id.
+		r.Put("/", rs.Update)    // PUT /posts/{id} - Update a single post by :id.
+		r.Delete("/", rs.Delete) // DELETE /posts/{id} - Delete a single post by :id.
+	})
+
+	return r
+}
+
+// Request Handler - GET /posts - Read a list of posts.
+func (rs postsResource) List(w http.ResponseWriter, r *http.Request) {
+	cows := make([]Cow, len(rs.store))
+
+	rs.Lock()
+	i := 0
+	for _, cow := range rs.store {
+		cows[i] = cow
+		i++
+	}
+	rs.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonBytes, err := json.Marshal(cows)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+// Request Handler - POST /posts - Create a new post.
+func (rs postsResource) Create(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	var cow Cow
+	err = json.Unmarshal(bodyBytes, &cow)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("Needed content-type: application/json but got '%s'",
+			ct)))
+	}
+
+	rs.Lock()
+	rs.store[cow.ID] = cow
+	defer rs.Unlock()
+}
+
+func PostCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "id", chi.URLParam(r, "id"))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// Request Handler - GET /posts/{id} - Read a single post by :id.
+func (rs postsResource) Get(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("id").(string)
+	found := rs.store[id]
+
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonBytes, err := json.Marshal(found)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+// Request Handler - PUT /posts/{id} - Update a single post by :id.
+func (rs postsResource) Update(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("id").(string)
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+
+	fmt.Println("ID %s", id)
+
+	var cow Cow
+	err = json.Unmarshal(bodyBytes, &cow)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+	rs.store[id] = cow
+}
+
+// Request Handler - DELETE /posts/{id} - Delete a single post by :id.
+func (rs postsResource) Delete(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("id").(string)
+	delete(rs.store, id)
+}
--- server.go
+++ server.go
@@ -1,101 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"sync"
+
+	"github.com/go-chi/chi/v5"
 )
 
-type Cow struct {
-	Name   string `json:"name"`
-	ID     string `json:"id"`
-	Date   string `json:"date"`
-	Image  string `json:"image"`
-	Finder string `json:"finder"`
-}
-
-type cowHandlers struct {
-	sync.Mutex
-	store map[string]Cow
-}
-
-func (h *cowHandlers) cows(w http.ResponseWriter, r *http.Request) {
-	//Allow CORS here By * or specific origin
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	switch r.Method {
-	case "GET":
-		h.get(w, r)
-		return
-	case "POST":
-		h.post(w, r)
-		return
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Unsupported Method."))
-		return
-	}
-}
-
-func (h *cowHandlers) get(w http.ResponseWriter, r *http.Request) {
-	cows := make([]Cow, len(h.store))
-
-	h.Lock()
-	i := 0
-	for _, cow := range h.store {
-		cows[i] = cow
-		i++
-	}
-	h.Unlock()
-
-	jsonBytes, err := json.Marshal(cows)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-	}
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-}
-
-func (h *cowHandlers) post(w http.ResponseWriter, r *http.Request) {
-	bodyBytes, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-	}
-
-	var cow Cow
-	err = json.Unmarshal(bodyBytes, &cow)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-	}
-
-	ct := r.Header.Get("content-type")
-	if ct != "application/json" {
-		w.WriteHeader(http.StatusUnsupportedMediaType)
-		w.Write([]byte(fmt.Sprintf("Needed content-type: application/json but got '%s'",
-			ct)))
-	}
-
-	h.Lock()
-	h.store[cow.ID] = cow
-	defer h.Unlock()
-}
-
-func newCowHandlers() *cowHandlers {
-	return &cowHandlers{
-		store: map[string]Cow{},
-	}
-}
-
 func main() {
-	cowHandlers := newCowHandlers()
-	http.HandleFunc("/v0/cows", cowHandlers.cows)
-	http.ListenAndServe(":8080", nil)
+	port := "8080"
 
+	log.Printf("Starting up on http://localhost:%s", port)
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("Hello World!"))
+	})
+
+	h := newHandler()
+	r.Mount("/posts", h.Routes())
+
+	log.Fatal(http.ListenAndServe(":" + port, r))
 }
